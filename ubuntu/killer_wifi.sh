1d5d9eae80704e3c521136abca5c7bf1db61a4b8
Shital Shah <shitals@microsoft.com>
2019-11-09 23:41:34 -0800

    rerunnable killer wifi

--- ubuntu/killer_wifi.sh
+++ ubuntu/killer_wifi.sh
@@ -8,12 +8,12 @@ sudo apt-get install -y git
 sudo apt-get install -y build-essential
 
 cd ~/GitHubSrc
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
+[ ! -d 'linux-firmware' ] && git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 cd linux-firmware
 sudo cp iwlwifi-* /lib/firmware/
 cd ..
 
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/backport-iwlwifi.git
+[ ! -d 'backport-iwlwifi' ] && git clone https://git.kernel.org/pub/scm/linux/kernel/git/iwlwifi/backport-iwlwifi.git
 cd backport-iwlwifi
 sudo make defconfig-iwlwifi-public
 sudo make -j4
