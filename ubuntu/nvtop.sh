957cb1e945e21209b811537fb743abad18a3b49f
Shital Shah <shitals@microsoft.com>
2019-11-13 21:11:55 -0800

    added nvtop

---
+++ ubuntu/nvtop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#fail if any errors
+set -e
+set -o xtrace
+
+# Ubuntu 19 onwards nvtop can be installed by sudo apt
+
+mkdir -p ~/GitHubSrc
+pushd ~/GitHubSrc
+
+sudo apt -y install cmake libncurses5-dev libncursesw5-dev git
+
+# clone if not already exist
+[ ! -d 'nvtop' ] && git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+
+# if conda environment then first deactivate it
+env="${CONDA_DEFAULT_ENV}"
+if [[ ! -z "${env}" ]]; then
+    conda deactivate
+
+cmake ..
+
+# If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
+# try the following command instead, otherwise skip to the build with make.
+cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+
+make
+sudo make install # You may need sufficient permission for that (root)
+
+# reactivate conda environment
+if [[ ! -z "${env}" ]]; then
+    conda activate "${env}"
+
+popd
