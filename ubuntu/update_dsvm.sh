824ccf189cd77fdf41f627c4c9b341094545e83e
Shital Shah <sytelus@gmail.com>
2019-12-11 04:16:40 -0800

    don't install latest cuda on dsvm

--- ubuntu/update_dsvm.sh
+++ ubuntu/update_dsvm.sh
@@ -3,6 +3,10 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y cuda-drivers
-sudo apt-get install -y cuda
+#sudo apt-get install -y cuda
+
+#sudo apt-get install -y cuda-10-0
+#conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
+pip install -q --upgrade torch
 echo --------------------------
 echo !!Please reboot!!
