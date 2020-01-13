e35b3fb523d6725564aab5659604cfe76f4f74e4
Shital Shah <shitals@microsoft.com>
2020-01-12 19:27:32 -0800

    add cuda, pytorch version

--- ubuntu/sysinfo.sh
+++ ubuntu/sysinfo.sh
@@ -9,3 +9,12 @@ echo -e "\n *** GPU Info ***\n"
 nvidia-smi -q
 echo -e "\n *** NVlink Info ***\n"
 nvidia-smi nvlink --capabilities
+echo -e "\n *** Conda Info ***\n"
+conda info
+echo -e "\n *** Conda Info ***\n"
+nvcc --version
+whereis cuda
+whereis cudnn.h
+cat $(whereis cudnn.h) | grep CUDNN_MAJOR -A 2
+cat $(whereis cuda)/include/cudnn.h | grep CUDNN_MAJOR -A 2
+python -c "import torch; print('Pytorch Ver:', torch.__version__)"
