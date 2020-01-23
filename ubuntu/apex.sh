b69b7f50f8c10a762712bae762927b0190d2b828
Shital Shah <shitals@microsoft.com>
2020-01-22 16:18:45 -0800

    re-runnable

--- ubuntu/apex.sh
+++ ubuntu/apex.sh
@@ -3,9 +3,20 @@
 set -e
 set -o xtrace
 
-mkdir -p ~/GitHubSrc
-pushd ~/GitHubSrc
-git clone https://github.com/NVIDIA/apex
-cd apex
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-popd
+if python -c "import apex" &> /dev/null; then
+    echo 'NVidia Apex is already installed'
+else
+    mkdir -p ~/GitHubSrc
+    pushd ~/GitHubSrc
+    git clone https://github.com/NVIDIA/apex
+    cd apex
+    pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+    popd
+fi
+
+if python -c "import nvidia.dali.pipeline" &> /dev/null; then
+    echo 'NVidia dali is already installed'
+else
+    pip install --extra-index-url https://developer.download.nvidia.com/compute/redist/cuda/10.0 nvidia-dali
+fi
+
