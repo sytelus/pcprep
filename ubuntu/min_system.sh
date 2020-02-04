9c693c3f54f05eddb77cd19ab5666e22b86e9de7
Shital Shah <shitals@microsoft.com>
2020-02-04 02:18:48 -0800

    added spotify.desktop, locate

--- ubuntu/min_system.sh
+++ ubuntu/min_system.sh
@@ -4,5 +4,5 @@ set -e
 set -o xtrace
 
 sudo apt-get -y update
-sudo apt-get -y install git curl wget xclip xz-utils tar apt-transport-https trash-cli bash-completion fslint fdupes
+sudo apt-get -y install git curl wget xclip xz-utils tar apt-transport-https trash-cli bash-completion fslint fdupes locate
 sudo apt-get -y install tlp powertop tlp-rdw inxi procinfo nvidia-prime htop #conky-all #conky-cli
