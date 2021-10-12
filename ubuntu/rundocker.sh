45f9d74ea438b3e661e9748bb3b3693ecaaaab79
Shital Shah <shitals@microsoft.com>
2021-10-12 00:58:22 -0700

    fixed line break

--- ubuntu/rundocker.sh
+++ ubuntu/rundocker.sh
@@ -2,7 +2,7 @@ docker run --gpus all -d --name archai \
     -u $(id -u):$(id -g) \
     -e HOME=$HOME -e USER=$USER \
     -v $HOME:$HOME \
-    -v /dataroot:$HOME/dataroot
+    -v /dataroot:$HOME/dataroot \
     --ipc=host \
     --net=host \
     -it sytelus/archai
