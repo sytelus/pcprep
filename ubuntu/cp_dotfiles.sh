1b555438fd36bafa4f924a165af42b57dc402960
Shital Shah <shitals@microsoft.com>
2023-02-01 00:28:20 -0800

    update cp dot

--- ubuntu/cp_dotfiles.sh
+++ ubuntu/cp_dotfiles.sh
@@ -7,8 +7,8 @@ cp -f .bash_aliases ~/.bash_aliases
 cp -f .inputrc ~/.inputrc
 cp -f .tmux.conf ~/.tmux.conf
 cp -f rundocker.sh ~/rundocker.sh
-cp -f azure-mount-config.yaml /azure-mount-config.yaml
-cp -f azure_mount_blob.sh ~/azure_mount_blob.sh
+cp -f azmount.yaml /azmount.yaml
+cp -f azmount.sh ~/azmount.sh
 sudo chmod +x ~/rundocker.sh
 cp -rf .config/ ~/
 cp -rf .local/ ~/
