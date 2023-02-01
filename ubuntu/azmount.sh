0bb7b0a4a74a23ce110ee97682b51d9439cfeefd
Shital Shah <shitals@microsoft.com>
2023-02-01 00:34:57 -0800

    fix azmount.sh

--- ubuntu/azmount.sh
+++ ubuntu/azmount.sh
@@ -11,4 +11,4 @@ mkdir -p /tmp/blobfuse # cache location for Azure blob storage
 # Example: https://github.com/Azure/azure-storage-fuse/blob/main/sampleFileCacheConfig.yaml
 # Full Config: https://github.com/Azure/azure-storage-fuse/blob/main/setup/baseConfig.yaml
 # sudo is required
-sudo blobfuse2 mount ~/azblob --config-file=./azure-mount-config.yaml
+sudo blobfuse2 mount ~/azblob --config-file=./azmount.yaml
