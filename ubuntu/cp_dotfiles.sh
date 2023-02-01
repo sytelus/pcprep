e22f2808352092bc9b4cf0bb6a0a0da2b58a47b2
Shital Shah <shitals@microsoft.com>
2023-02-01 00:29:14 -0800

    fix cp dot

--- ubuntu/cp_dotfiles.sh
+++ ubuntu/cp_dotfiles.sh
@@ -7,7 +7,7 @@ cp -f .bash_aliases ~/.bash_aliases
 cp -f .inputrc ~/.inputrc
 cp -f .tmux.conf ~/.tmux.conf
 cp -f rundocker.sh ~/rundocker.sh
-cp -f azmount.yaml /azmount.yaml
+cp -f azmount.yaml ~/azmount.yaml
 cp -f azmount.sh ~/azmount.sh
 sudo chmod +x ~/rundocker.sh
 cp -rf .config/ ~/
