a927b26d806f75e196fba00cb3f7dae4829dad63
Shital Shah <shitals@microsoft.com>
2023-06-21 02:58:38 -0700

    added keychain and gpustat

--- ubuntu/min_system.sh
+++ ubuntu/min_system.sh
@@ -9,8 +9,8 @@ sudo apt-get install --assume-yes --no-install-recommends \
       tlp powertop tlp-rdw inxi procinfo nvidia-prime htop #conky-all #conky-cli \
       build-essential cmake libopencv-dev g++ libopenmpi-dev zlib1g-dev \
       fortune-mod sl espeak figlet sysvbanner cowsay oneko cmatrix toilet pi xcowsay aview bb rig weather-util ponysay \
-      fslint fdupes locate \
-      apt-transport-https ca-certificates curl gnupg lsb-release \
+      fslint fdupes locate keychain \
+      apt-transport-https ca-certificates curl gnupg lsb-release gpustat \
       bzip2 libglib2.0-0 libxext6 libsm6 libxrender1 mercurial subversion \
       nvtop virt-what sudo zlib1g g++ freeglut3-dev build-essential libx11-dev \
       libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage3 libfreeimage-dev
