969d053a313373c3ee132c29eaf888c5262f8333
Shital Shah <shitals@microsoft.com>
2023-07-20 21:08:57 -0700

    update home scripts

--- ubuntu/cp_dotfiles.sh
+++ ubuntu/cp_dotfiles.sh
@@ -7,10 +7,18 @@ cp -f .bash_aliases ~/.bash_aliases
 cp -f .inputrc ~/.inputrc
 cp -f .tmux.conf ~/.tmux.conf
 cp -f rundocker.sh ~/rundocker.sh
+
 cp -f azmount.yaml ~/azmount.yaml
 cp -f azmount.sh ~/azmount.sh
-sudo chmod +x ~/rundocker.sh
+
+cp -f mount_cifs.sh ~/mount_cifs.sh
+cp -f start_tmux.sh ~/start_tmux.sh
+cp -f sysinfo.sh ~/sysinfo.sh
+cp -f treesize.sh ~/treesize.sh
+
+cp -f measure_flops.py ~/measure_flops.py
 cp -rf .config/ ~/
 cp -rf .ssh/ ~/
 cp -rf .local/ ~/
 
+sudo chmod +x ~/*.sh
