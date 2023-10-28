5a4bfad78782988b4370cf276d7a107c1352c157
Shital Shah <shitals@microsoft.com>
2023-10-28 05:13:52 -0700

    add -y for install

--- ubuntu/install_cudnn.sh
+++ ubuntu/install_cudnn.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y zlib1g
 sudo dpkg -i ~/cudnn-local-repo-ubuntu2004-8.9.5.30_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-*/cudnn-local-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get -y update
-sudo apt-get install libcudnn8=8.9.5.30_1+cuda12.1
-sudo apt-get install libcudnn8-dev=8.9.5.30_1+cuda12.1
-sudo apt-get install libcudnn8-samples=8.9.5.30_1+cuda12.1
+sudo apt-get -y install libcudnn8=8.9.5.30_1+cuda12.1
+sudo apt-get -y install libcudnn8-dev=8.9.5.30_1+cuda12.1
+sudo apt-get -y install libcudnn8-samples=8.9.5.30_1+cuda12.1
 rm ~/cudnn-local-repo-ubuntu2004-8.9.5.30_1.0-1_amd64.deb
