877bef862ddf94c150b4d156e2563aad7201c0dd
Shital Shah <shitals@microsoft.com>
2023-10-28 05:03:55 -0700

    better cuda install

--- ubuntu/install_cuda_all.sh
+++ ubuntu/install_cuda_all.sh
@@ -1,7 +1,7 @@
 # Below is better way
 
 # conda install cuda -c nvidia/label/cuda-12.1.0
-# conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia/label/cuda-12.1.0
+# conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia/label/cuda-12.1.0
 
 
 # CUDA 12.2
@@ -16,33 +16,28 @@
 # To uninstall previous versions use uninstall_cuda.sh
 # To solve error: E: Conflicting values set for option Signed-By regarding source https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /: /usr/share/keyrings/cuda-archive-keyring.gpg !=
 # sudo mv /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list.old
-# CUDA 11.8
+# CUDA 12.1
 # DO NOT INSTALL nighly PyTorch
-# Current PyTorch stable version do not support > CUDA 11.8
-# CUDA 11.8 must be installed using local deb for ease of later removal.
+# Current PyTorch stable version do not support > CUDA 12.1
+# CUDA 12.1 must be installed using local deb for ease of later removal.
 # DO NOT use network deb as it installs 12.2 which is not supported by PyTorch
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get -y install cuda
+
+bash install_cudatoollkit.sh
 
 # cuDNN install
 sudo apt-get install -y zlib1g
-wget https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.5/local_installers/11.x/cudnn-local-repo-ubuntu2004-8.9.5.29_1.0-1_amd64.deb/
+wget https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.5/local_installers/12.x/cudnn-local-repo-ubuntu2004-8.9.5.30_1.0-1_amd64.deb/
 sudo dpkg -i cudnn-local-repo-ubuntu2004-8.9.5.29_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-*/cudnn-local-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get -y update
-sudo apt-get install libcudnn8=8.9.5.29-1+cuda11.8
-sudo apt-get install libcudnn8-dev=8.9.5.29-1+cuda11.8
-sudo apt-get install libcudnn8-samples=8.9.5.29-1+cuda11.8
+sudo apt-get install libcudnn8=8.9.5.29-1+cuda12.1
+sudo apt-get install libcudnn8-dev=8.9.5.29-1+cuda12.1
+sudo apt-get install libcudnn8-samples=8.9.5.29-1+cuda12.1
 rm cudnn-local-repo-ubuntu2004-8.9.5.29_1.0-1_amd64.deb
 
-#Above might still install 12.2. Use below to install 11.8
+#Above might still install 12.2. Use below to install 12.1
 sudo apt-get install cuda-toolkit-11-8
-export CUDA_HOME=/usr/local/cuda-11.8/ # goes in .bashrc
+export CUDA_HOME=/usr/local/cuda-12.1/ # goes in .bashrc
 
 
 # Use local CUDA version instead of one in /usr/bin
--- ubuntu/uninstall_cuda.sh
+++ ubuntu/uninstall_cuda.sh
@@ -3,14 +3,14 @@
 # To uninstall previous CUDA versions
 # nvidia-* and libnvidia-* removes drivers also. Better to remove everything and reinstall.
 # libcudnn8* removed cuDNN
-sudo apt-get --purge remove cuda-* nvidia-* gds-tools-* libcublas-* libcufft-* libcufile-* libcurand-* libcusolver-* libcusparse-* libnpp-* libnvidia-* libnvjitlink-* libnvjpeg-* nsight* nvidia-* libnvidia-* libcudnn8*
+sudo apt-get -y --purge remove cuda-* nvidia-* gds-tools-* libcublas-* libcufft-* libcufile-* libcurand-* libcusolver-* libcusparse-* libnpp-* libnvidia-* libnvjitlink-* libnvjpeg-* nsight* nvidia-* libnvidia-* libcudnn8*
 
 # For older version also run below
-sudo apt-get --purge remove "*cublas*" "*cufft*" "*curand*" "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*"
+sudo apt-get -y --purge remove "*cublas*" "*cufft*" "*curand*" "*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*"
 
 # cleanup uninstall
-sudo apt-get autoremove
-sudo apt-get autoclean
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
 
 # remove cuda directories
 sudo rm -rf /usr/local/cuda*
