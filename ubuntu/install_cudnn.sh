d2d83529d49c21a3ac046585b101258aa976473f
Shital Shah <shitals@microsoft.com>
2023-12-06 19:15:27 -0800

    cudnn compatible with cuda 12.1

--- ubuntu/install_cudnn.sh
+++ ubuntu/install_cudnn.sh
@@ -3,12 +3,14 @@
 set -e
 set -o xtrace
 
+# sudo apt-cache madison libcudnn8
+
 sudo apt-get install -y zlib1g
-# wget https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.7/local_installers/12.x/cudnn-local-repo-ubuntu2004-8.9.7.29_1.0-1_amd64.deb/
-sudo dpkg -i ~/cudnn-local-repo-ubuntu2004-8.9.7.29_1.0-1_amd64.deb
+# wget https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.3/local_installers/12.x/cudnn-local-repo-ubuntu2004-8.9.3.28_1.0-1_amd64.deb/
+sudo dpkg -i ~/cudnn-local-repo-ubuntu2004-8.9.3.28_1.0-1_amd64.deb
 sudo cp /var/cudnn-local-repo-*/cudnn-local-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get -y update
-sudo apt-get -y install libcudnn8=8.9.7.29_1+cuda12.1
-sudo apt-get -y install libcudnn8-dev=8.9.7.29_1+cuda12.1
-sudo apt-get -y install libcudnn8-samples=8.9.7.29_1+cuda12.1
-# rm ~/cudnn-local-repo-ubuntu2004-8.9.7.29_1.0-1_amd64.deb
+sudo apt-get -y install libcudnn8=8.9.3.28-1+cuda12.1
+sudo apt-get -y install libcudnn8-dev=8.9.3.28-1+cuda12.1
+sudo apt-get -y install libcudnn8-samples=8.9.3.28-1+cuda12.1
+# rm ~/cudnn-local-repo-ubuntu2004-8.9.3.28_1.0-1_amd64.deb
