86409f849937b0fb646f0981a1ecebf3ab67c532
Shital Shah <sytelus@gmail.com>
2024-10-13 20:01:38 -0700

    separated mini conda install

--- ubuntu/install_full_sandbox.sh
+++ ubuntu/install_full_sandbox.sh
@@ -39,11 +39,7 @@ bash gitconfig.sh
 bash install_fzf.sh
 
 
-# install mini conda with Python 3.11 (3.12 has breaking changes with imp module)
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-py311_24.5.0-0-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
+bash install_miniconda.sh
 
 # modify .bashrc
 ~/miniconda3/bin/conda init bash
---
+++ ubuntu/install_miniconda.sh
@@ -0,0 +1,5 @@
+# install mini conda with Python 3.11 (3.12 has breaking changes with imp module)
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-py311_24.7.1-0-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
