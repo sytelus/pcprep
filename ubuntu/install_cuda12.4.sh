0a0f9d3eafe1118222fb3079b2abe5c5111d1725
Shital Shah <sytelus@gmail.com>
2024-10-14 01:08:47 -0700

    added cuDNN install

--- ubuntu/install_cuda12.4.sh
+++ ubuntu/install_cuda12.4.sh
@@ -41,4 +41,11 @@ else
     sudo apt-get -y install nvidia-gds
 fi
 
+# install cuDNN
+wget https://developer.download.nvidia.com/compute/cudnn/9.5.0/local_installers/cudnn-local-repo-ubuntu2204-9.5.0_1.0-1_amd64.deb
+sudo dpkg -i cudnn-local-repo-ubuntu2204-9.5.0_1.0-1_amd64.deb
+sudo cp /var/cudnn-local-repo-ubuntu2204-9.5.0/cudnn-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cudnn-cuda-12
+
 echo "Must reboot the machine!"
