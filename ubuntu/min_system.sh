f63a11090c33331ffc789aa861ed42f7de406f00
Shital Shah <sytelus@gmail.com>
2024-11-09 10:48:17 +0000

    add docker installation

--- ubuntu/min_system.sh
+++ ubuntu/min_system.sh
@@ -23,49 +23,74 @@ esac
 
 # Attempt to run a harmless command with sudo
 if sudo -n true 2>/dev/null; then
-      # install core packages
-      sudo apt-get install --assume-yes --no-install-recommends \
-            git curl wget xclip xz-utils tar apt-transport-https trash-cli bash-completion \
-            tlp powertop tlp-rdw inxi procinfo htop aptitude \
-            build-essential cmake libopencv-dev g++ libopenmpi-dev zlib1g-dev \
-            fortune-mod sl espeak figlet sysvbanner cowsay oneko cmatrix toilet pi xcowsay aview bb rig weather-util \
-            fdupes locate keychain pass micro zlib1g \
-            apt-transport-https ca-certificates curl gnupg lsb-release  \
-            bzip2 libglib2.0-0 libxext6 libsm6 libxrender1 mercurial subversion \
-            virt-what sudo zlib1g g++ freeglut3-dev build-essential libx11-dev \
-            libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage3 \
-            libfreeimage-dev vmtouch neofetch powerstat powertop
+    # install core packages
+    sudo apt-get install --assume-yes --no-install-recommends \
+        git curl wget xclip xz-utils tar apt-transport-https trash-cli bash-completion \
+        tlp powertop tlp-rdw inxi procinfo htop aptitude \
+        build-essential cmake libopencv-dev g++ libopenmpi-dev zlib1g-dev \
+        fortune-mod sl espeak figlet sysvbanner cowsay oneko cmatrix toilet pi xcowsay aview bb rig weather-util \
+        fdupes locate keychain pass micro zlib1g \
+        apt-transport-https ca-certificates curl gnupg lsb-release  \
+        bzip2 libglib2.0-0 libxext6 libsm6 libxrender1 mercurial subversion \
+        virt-what sudo zlib1g g++ freeglut3-dev build-essential libx11-dev \
+        libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage3 \
+        libfreeimage-dev vmtouch neofetch powerstat powertop
 
-      # Install nvidia-prime only on x86_64 architecture
-      if [ "$ARCH" = "x86_64" ]; then
-          sudo apt-get install --assume-yes nvidia-prime
-      fi
+    # Install nvidia-prime only on x86_64 architecture
+    if [ "$ARCH" = "x86_64" ]; then
+        sudo apt-get install --assume-yes nvidia-prime
+    fi
 
-      # update stdc, without this pytest discovery fails
-      sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-      sudo apt-get -y update
-      sudo apt install -y gcc
-      sudo apt-get install -y --only-upgrade libstdc++6
+    # update stdc, without this pytest discovery fails
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get -y update
+    sudo apt install -y gcc
+    sudo apt-get install -y --only-upgrade libstdc++6
 
-      # Check if Azure CLI is installed
-      if ! command -v az &> /dev/null; then
-          echo "Azure CLI not found. Installing..."
-          curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-      fi
+    # Check if Azure CLI is installed
+    if ! command -v az &> /dev/null; then
+        echo "Azure CLI not found. Installing..."
+        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+    fi
 
-      # perms to install az extensions
-      az config set extension.use_dynamic_install=yes_without_prompt
-      sudo mkdir -p /opt/az/extensions/
-      sudo chmod 777 /opt/az/extensions/
+    # perms to install az extensions
+    az config set extension.use_dynamic_install=yes_without_prompt
+    sudo mkdir -p /opt/az/extensions/
+    sudo chmod 777 /opt/az/extensions/
 
-      # GitHub CLI
-      (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-      && sudo mkdir -p -m 755 /etc/apt/keyrings \
-      && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-      && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-      && echo "deb [arch=$ARCH_DEB signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-      && sudo apt update \
-      && sudo apt install gh -y
+    # GitHub CLI
+    (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+    && sudo mkdir -p -m 755 /etc/apt/keyrings \
+    && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$ARCH_DEB signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+
+    # Check if Docker is installed
+    if ! command -v docker &> /dev/null; then
+        echo "Docker not found. Installing..."
+        # Add Docker's official GPG key:
+        sudo apt-get update
+        sudo apt-get install ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+        sudo usermod -aG docker $USER
+        newgrp docker
+    else
+        echo "Docker is already installed."
+    fi
 else
     echo "Sudo access is not available."
 fi
@@ -82,3 +107,4 @@ if [ "$ARCH" = "x86_64" ]; then
 else
     echo "Skipping rusage installation - not available for $ARCH architecture"
 fi
+
