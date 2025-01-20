26f4f24c7a029dfd0f646ab788da57ab3ba54f03
Shital Shah <sytelus@gmail.com>
2025-01-20 01:05:19 -0800

    web_install script

--- ubuntu/web_install.sh
+++ ubuntu/web_install.sh
@@ -1,5 +1,12 @@
+# run this as:
+# curl -s https://github.com/sytelus/pcprep/blob/master/ubuntu/web_install.sh | bash
 
+set -eu -o pipefail -o xtrace # fail if any command failes, log all commands, -o xtrace
 
+pushd ~
 mkdir -p GitHubSrc
 cd GitHubSrc
-git clone
+git clone https://github.com/sytelus/pcprep.git
+cd pcprep/ubuntu
+bash prepare_new_box.sh
+popd
